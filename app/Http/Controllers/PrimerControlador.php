68d0b75ae47607fddade1c3433e97e93e2871b79
jccj <jccjnms@gmail.com>
2025-01-28 00:21:21 -0600

    Migraciones

---
+++ app/Http/Controllers/PrimerControlador.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrimerControlador extends Controller
+{
+    function index(){
+        $posts = ['post1', 'post2'];
+        //return view('contact',['posts' => $posts]);
+        return view('contact',compact('posts'));
+    }
+
+    function otro($post){
+        echo($post);
+    }
+}
